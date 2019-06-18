eda0d04c134e05d1a7e0b2c499deba877e0ed8e6
Ian Brooks <brooks_ian@yahoo.com>
2019-06-18 16:36:42 -0400

    Update Repo

---
+++ buildModel.sh
@@ -0,0 +1,58 @@
+#Dataset Prep
+
+##Convert XML Labels to CSV
+# From Home Directory
+cd
+python xml_to_csv.py -i Images/train -o annotations/train_labels.csv
+python xml_to_csv.py -i Images/test -o annotations/test_labels.csv
+
+##Convert CSV to TF-Record
+# From Home Directory
+cd
+python3 generate_tfrecord.py --label0=Cloudera --label1=Hortonworks --csv_input=annotations/train_labels.csv --img_path=Images/train  --output_path=annotations/train.record
+python3 generate_tfrecord.py --label0=Cloudera --label1=Hortonworks --csv_input=annotations/test_labels.csv --img_path=Images/test  --output_path=annotations/test.record
+
+#Train Tensorflow Model
+
+#From Home Directory
+cd
+python3 train.py --logtostderr --train_dir=training/ \
+--pipeline_config_path=training/ssd_inception_v2_coco.config
+
+#Find the Highest Ranked Checkpoint File
+#ls -t ~/training/model.ckpt*
+#Make a note of the file’s name, as it will be passed as an argument when we call the export_inference_graph.py script.
+
+
+
+#python3 tensorflow/models/research/object_detection/export_tflite_ssd_graph.py \
+#    --input_type=image_tensor \
+#    --input_shape={"image_tensor":[1,600,600,3]} \
+#    --pipeline_config_path=trained-inference-graphs/output_inference_graph_v1/pipeline.config \
+#    --trained_checkpoint_prefix=trained-inference-graphs/output_inference_graph_v1/model.ckpt \
+#    --output_directory=trainedTFLite \
+#    --add_postprocessing_op=true \
+#    --max_detections=10
+
+#Check Model 
+#saved_model_cli show --dir trained-inference-graphs/output_inference_graph_v1/saved_model --all
+
+#Convert with TOCO
+
+#Convert TF Graphs to TFLite Model
+#toco --output_file=LogoObjD.tflite \
+#  --graph_def_file=trainedTFLite/tflite_graph.pb \
+#  --input_format=TENSORFLOW_GRAPHDEF \
+#  --inference_input_type=QUANTIZED_UINT8 \
+#  --inference_type=QUANTIZED_UINT8 \
+#  --input_arrays=normalized_input_image_tensor \
+#  --output_arrays='TFLite_Detection_PostProcess','TFLite_Detection_PostProcess:1','TFLite_Detection_PostProcess:2','TFLite_Detection_PostProcess:3'  \
+#  --output_format=TFLITE \
+#  --input_shape=1,300,300,3 \
+#  --input_data_type=QUANTIZED_UINT8 \
+#  --mean_values=128 \
+#  --std_dev_values=128 \
+#  --default_ranges_min=0.0 \
+#  --default_ranges_max=300 \
+#  --allow_custom_ops 
+
---
+++ setup.sh
@@ -0,0 +1,57 @@
+#Install Tensorflow
+pip3 install tensorflow
+pip install tensorflow
+
+#Prerequisite for Tensorflow Models
+pip3 install pillow
+pip3 install lxml
+pip3 install jupyter
+pip3 install matplotlib
+pip3 install opencv
+
+#Clone Tensorflow Model Git Repo and Build Project
+mkdir tensorflow
+cd tensorflow
+git clone https://github.com/tensorflow/models.git
+cd /home/cdsw/tensorflow/models/research
+python setup.py build
+python setup.py install
+
+#Download Original Tensorflow Model
+cd
+mkdir pre-trained-model
+cd pre-trained-model
+wget http://download.tensorflow.org/models/object_detection/ssd_inception_v2_coco_2018_01_28.tar.gz
+tar -xzf ssd_inception_v2_coco_2018_01_28.tar.gz
+
+#Install Tensorflow - Object Detection Tools
+
+#COCO API Install
+git clone https://github.com/cocodataset/cocoapi.git
+cd cocoapi/PythonAPI
+make
+cp -r pycocotools ~/tensorflow/models/research/
+cd ~/tensorflow/models/research/
+#protoc object_detection/protos/*.proto --python_out=.
+
+#Download Protobuffer Writers
+# From tensorflow/models/research/
+cd ~/tensorflow/models/research
+wget -O protobuf.zip https://github.com/google/protobuf/releases/download/v3.0.0/protoc-3.0.0-linux-x86_64.zip
+unzip protobuf.zip
+
+# From tensorflow/models/research/
+cd ~/tensorflow/models/research
+./bin/protoc object_detection/protos/*.proto --python_out=.
+
+# From tensorflow/models/research/
+cd ~/tensorflow/models/research
+export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
+
+#Export Inference Graph From Home Directory
+cd
+export PYTHONPATH=$PYTHONPATH:~/tensorflow/models/research/
+export PYTHONPATH=$PYTHONPATH:~/tensorflow/models/research/slim
+export PYTHONPATH=$PYTHONPATH:~/tensorflow/models/research/object_detection
+
+export PATH=$PATH:~/.local/bin 
