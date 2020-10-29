c9bb7dbd645069fdb4f1d246a389c9650f39a8bc
Ian Brooks <brooks_ian@yahoo.com>
2020-10-29 16:59:53 -0400

    Update exportModelJS.sh

--- scripts/exportModelJS.sh
+++ scripts/exportModelJS.sh
@@ -3,10 +3,7 @@ echo '+++++ Export Model to JS +++++'
 
 #conda activate tensorflow
 
-pip install tensorflowjs
-
 cd
-
 tensorflowjs_converter \
     --input_format=tf_saved_model \
     --output_node_names='MobilenetV1/Predictions/Reshape_1' \
